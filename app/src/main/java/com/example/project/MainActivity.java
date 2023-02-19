aa34294b404862d0dd18c6a584cace6f61c6fe9f
Ivana Sliško <ina23125@gmail.com>
2023-02-19 21:50:09 +0100

    added changes

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.project;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.project.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        HomeFragment homeFragment = new HomeFragment();
+        LearnMoreFragment learnMoreFragment = new LearnMoreFragment();
+        InfoFragment infoFragment = new InfoFragment();
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, homeFragment).commit();
+
+        binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case (R.id.home_icon):
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame, homeFragment).commit();
+                        return true;
+
+                    case (R.id.learnMore_icon):
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame, learnMoreFragment).commit();
+                        return true;
+
+                    case (R.id.info_icon):
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame, infoFragment).commit();
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+
+
     }
 }
